de04c7526c83e9d24376b728f22537f1403fe836
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-03-06 09:17:06 -0800

    added content type

--- api/server.js
+++ api/server.js
@@ -31,7 +31,8 @@ server.use(cors(corsOptionsDelegate));
 server.use(helmet());
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://essentialism.teagueteam.now.sh/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Content-Type', 'application/json');
     next();
   });
 
