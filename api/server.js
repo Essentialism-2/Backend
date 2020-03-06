1a47e8cfcaa0edaacdedfdc54f7ddc095af605e3
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-03-06 08:57:44 -0800

    added https://essentialism.teagueteam.now.sh  to cors thing

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ server.use(helmet());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://essentialism.teagueteam.now.sh');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
