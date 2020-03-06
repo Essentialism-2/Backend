a811067fc9a08a74a4bce4c46036e66ba49679ce
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-03-06 09:34:14 -0800

    changes to cors

--- api/server.js
+++ api/server.js
@@ -31,7 +31,7 @@ server.use(cors(corsOptionsDelegate));
 server.use(helmet());
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 	res.header('Content-Type', 'application/json');
     next();
   });
